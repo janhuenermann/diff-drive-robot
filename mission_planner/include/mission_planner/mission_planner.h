65c3d1b8422a9b156e24d62b10b794322a187bba
Jan Hünermann <jan@huenermann.de>
2020-03-04 00:22:16 +0800

    Higher threshold

--- mission_planner/include/mission_planner/mission_planner.h
+++ mission_planner/include/mission_planner/mission_planner.h
@@ -12,7 +12,7 @@ private:
   ros::Subscriber pos_sub;
   ros::Publisher goal_pub;
   int current_goal;                             // index of current goal
-  const double THRESHOLD_DISTANCE = 0.1;        // min distance to goal to be considered reached
+  const double THRESHOLD_DISTANCE = 0.20;        // min distance to goal to be considered reached
 public:
   MissionPlanner(ros::NodeHandle *nh,std::vector<geometry_msgs::Pose2D> goal_col);
   double calc_dist(geometry_msgs::Pose2D p1,geometry_msgs::Pose2D p2);
