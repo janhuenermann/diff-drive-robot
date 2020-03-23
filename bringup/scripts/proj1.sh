eb849c260f2be67ef37791c1fff963dbbda94d3c
Jan Hünermann <jan@huenermann.de>
2020-03-23 14:10:31 +0800

    Added world argument to bringup, added proj2.sh

--- bringup/scripts/proj1.sh
+++ bringup/scripts/proj1.sh
@@ -1,50 +1,10 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SETUP_SH="$DIR/../../../devel/setup.bash"
+# SETUP_SH="$DIR/../../../devel/setup.bash"
 
-source $SETUP_SH
-echo "Sourced $SETUP_SH"
+# source $SETUP_SH
+# echo "Sourced $SETUP_SH"
 
-# all environment variables MUST NOT contain spaces
-# export GOALS="-4.0,-6.0|-2.5,-1.0|-9.0,1.5"
-# export X_POS="-8.9"
-# export Y_POS="-8.7"
-# export WORLD="world1"
-# export MIN_POS="-11.0,-11.0"
-# export MAX_POS="11.0,11.0"
-
-# ====== TURTLEBOT3_WORLD =========
-# export GOALS="0.5,0.5|1.5,0.5|-1.5,-1.5|-2.0,-0.5"
-# export X_POS="-2.0"
-# export Y_POS="-0.5"
-# export WORLD="turtlebot3_world"
-# export MIN_POS="-4.0,-4.0"
-# export MAX_POS="4.0,4.0"
-
-# ====== WORLD1 =========
-export GOALS="-4.0,-6.0|-2.5,-1.0|-9.0,1.5"
-export X_POS="-8.9"
-export Y_POS="-8.7"
-export WORLD="world1"
-export MIN_POS="-11.0,-11.0"
-export MAX_POS="11.0,11.0"
-
-# ====== WORLD2 =========
-# export GOALS="2.0,0.0|3.0,-4.0|-4.0,-6.0"
-# export X_POS="-2.0"
-# export Y_POS="-0.5"
-# export WORLD="world2"
-# export MIN_POS="-7.0,-11.0"
-# export MAX_POS="7.0,4.0"
-
-# ====== WORLD3 =========
-# export GOALS="3.0,-4.0|-1.0,-5.0|-0.5,-9.5"
-# export X_POS="-2.0"
-# export Y_POS="-0.5"
-# export WORLD="world3"
-# export MIN_POS="-6.0,-12.0"
-# export MAX_POS="6.0,6.0"
-
-export CELL_SIZE="0.05"
+. "$DIR/world_settings.sh" ${1:-0}
 roslaunch bringup robot_bringup.launch
---
+++ bringup/scripts/world_settings.sh
@@ -0,0 +1,36 @@
+export PROJ_GROUND_CELL_SIZE="0.05"
+
+case $1 in
+    1)
+        export PROJ_WORLD="world1"
+        export PROJ_GROUND_GOALS="-4.0,-6.0|-2.5,-1.0|-9.0,1.5"
+        export PROJ_GROUND_X_POS="-8.9"
+        export PROJ_GROUND_Y_POS="-8.7"
+        export PROJ_GROUND_MIN_POS="-11.0,-11.0"
+        export PROJ_GROUND_MAX_POS="11.0,11.0"
+        ;;
+    2)
+        export PROJ_WORLD="world2"
+        export PROJ_GROUND_GOALS="2.0,0.0|3.0,-4.0|-4.0,-6.0"
+        export PROJ_GROUND_X_POS="-2.0"
+        export PROJ_GROUND_Y_POS="-0.5"
+        export PROJ_GROUND_MIN_POS="-7.0,-11.0"
+        export PROJ_GROUND_MAX_POS="7.0,4.0"
+        ;;
+    3)
+        export PROJ_WORLD="world3"
+        export PROJ_GROUND_GOALS="3.0,-4.0|-1.0,-5.0|-0.5,-9.5"
+        export PROJ_GROUND_X_POS="-2.0"
+        export PROJ_GROUND_Y_POS="-0.5"
+        export PROJ_GROUND_MIN_POS="-6.0,-12.0"
+        export PROJ_GROUND_MAX_POS="6.0,6.0"
+        ;;
+    0)
+        export PROJ_WORLD="turtlebot3_world"
+        export PROJ_GROUND_GOALS="0.5,0.5|1.5,0.5|-1.5,-1.5|-2.0,-0.5"
+        export PROJ_GROUND_X_POS="-2.0"
+        export PROJ_GROUND_Y_POS="-0.5"
+        export PROJ_GROUND_MIN_POS="-4.0,-4.0"
+        export PROJ_GROUND_MAX_POS="4.0,4.0"
+        ;;
+esac
